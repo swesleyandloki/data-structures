1fe5f0dcaf86e2d49b6642c03358005c00920f9f
swesleyandloki <swesleyandloki@gmail.com>
2014-10-29 15:53:42 -0700

    Prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.pop = function(){
+  var popped = this.storage[this.size()-1];
+  delete this.storage[this.size()-1];
+  return popped;
+};
 
+stackMethods.push = function(item){
+  this.storage[this.size()] = item;
+};
+
+stackMethods.size = function(){
+  return Object.keys(this.storage).length;
+};
