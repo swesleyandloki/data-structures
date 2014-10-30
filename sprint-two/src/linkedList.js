66215e2e8437b1f735abf5e4fdd29b11a0c14d84
swesleyandloki <swesleyandloki@gmail.com>
2014-10-30 16:10:41 -0700

    linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,35 @@ var makeLinkedList = function(){
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value){
+    if(list.tail === null){
+      list.tail = makeNode(value);
+      list.head = list.tail;
+    } else {
+      list.tail.next = makeNode(value);
+      list.tail = list.tail.next;
+    }
+
+
   };
 
   list.removeHead = function(){
+    var head = list.head.value;
+    if (list.head !== null){
+      list.head = list.head.next;
+    }
+    return head;
   };
 
   list.contains = function(target){
+    var currNode = list.head;
+    var wasFound = false;
+    while(!wasFound && currNode !== null){
+      wasFound = (currNode['value'] === target);
+      currNode = currNode.next;
+    }
+    return wasFound;
   };
 
   return list;
@@ -26,4 +48,5 @@ var makeNode = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail and removeHead are constant, contains is linear.  we thinks.
  */
