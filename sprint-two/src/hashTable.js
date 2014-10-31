bb827e005226bf30c8e8ccba575de90822b42122
swesleyandloki <swesleyandloki@gmail.com>
2014-10-31 16:42:40 -0700

    completed hashtables finally

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,8 +2,8 @@ var HashTable = function(){
   this._limit = 2;
   this._storage = makeLimitedArray(this._limit);
   this._size = 0;
-  this.upperLimit = .75;
-  this.lowerLimit = 0.25
+  this.upperLimit = 0.75;
+  this.lowerLimit = 0.25;
 };
 
 HashTable.prototype.insert = function(k, v){
@@ -16,7 +16,10 @@ HashTable.prototype.insert = function(k, v){
     this._storage.set(i, bucket);
   }
   this._size ++;
-  this.reHash();
+  if ( this._size/this._limit > this.upperLimit ){
+    this._limit = this._limit * 2;
+    this.reHash();
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -38,57 +41,36 @@ HashTable.prototype.remove = function(k){
   if (bucket) {
     for(var j=0; j<bucket.length; j++){
       if(k===bucket[j][0]){
-      bucket.splice(j,1);
-      this._size --;
+        bucket.splice(j,1);
+        this._size --;
       }
     }
     if(bucket.length===0){
       bucket = null;
     }
     this._storage.set(i, bucket);
-    this.reHash();
-
+    if ( this._size/this._limit < this.lowerLimit && this._limit > 2  ){
+      this._limit = this._limit / 2;
+      this.reHash();
+    }
   }
 
 };
 
 HashTable.prototype.reHash = function(){
-  if ( this._size/this._limit > this.upperLimit ){
-    this._limit = this._limit * 2;
-
-    var insert = this.insert;
-    var oldStorage = this._storage;
-    this._storage = makeLimitedArray(this._limit);
-    oldStorage.each(function(el, key, list){
-      if( el !== undefined && el !== null){
-        console.log(list);
-        for( var i=0; i<el.length; i++ ){
-          //debugger;
-          insert.call(this,el[i]);
-        }
+  var that = this;
+  var oldStorage = this._storage;
+  this._storage = makeLimitedArray(this._limit);
+  this._size = 0;
+  oldStorage.each(function(el, key, list){
+    if( el !== undefined && el !== null){
+      //console.log(list);
+      for( var i=0; i<el.length; i++ ){
+        //debugger;
+        that.insert(el[i][0],el[i][1]);
       }
-    });
-
-  }
-  if ( this._size/this._limit < this.lowerLimit ){
-    this._limit = this._limit / 2;
-
-    var insert = this.insert;
-    var oldStorage = this._storage;
-    this._storage = makeLimitedArray(this._limit);
-    oldStorage.each(function(el, key, list){
-      if( el !== undefined && el !== null){
-        console.log(list);
-        for( var i=0; i<el.length; i++ ){
-          //debugger;
-          this.insert(el[i]);
-        }
-      }
-    });
-
-  }
-
-
+    }
+  });
 
 
 }
