be7f6443a2d4b9755a63cd39a1265ba4b10f5397
swesleyandloki <swesleyandloki@gmail.com>
2014-10-31 14:30:26 -0700

    almost done hasttable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,97 @@
 var HashTable = function(){
-  this._limit = 8;
+  this._limit = 2;
   this._storage = makeLimitedArray(this._limit);
+  this._size = 0;
+  this.upperLimit = .75;
+  this.lowerLimit = 0.25
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  if(bucket===undefined){
+    this._storage.set(i, [[k,v]]);
+  }else{
+    bucket.push([k,v]);
+    this._storage.set(i, bucket);
+  }
+  this._size ++;
+  this.reHash();
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  if ( bucket ){
+    for(var i=0; i<bucket.length; i++){
+      if(k===bucket[i][0]){
+        return bucket[i][1];
+      }
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  if (bucket) {
+    for(var j=0; j<bucket.length; j++){
+      if(k===bucket[j][0]){
+      bucket.splice(j,1);
+      this._size --;
+      }
+    }
+    if(bucket.length===0){
+      bucket = null;
+    }
+    this._storage.set(i, bucket);
+    this.reHash();
+
+  }
 
 };
+
+HashTable.prototype.reHash = function(){
+  if ( this._size/this._limit > this.upperLimit ){
+    this._limit = this._limit * 2;
+
+    var insert = this.insert;
+    var oldStorage = this._storage;
+    this._storage = makeLimitedArray(this._limit);
+    oldStorage.each(function(el, key, list){
+      if( el !== undefined && el !== null){
+        console.log(list);
+        for( var i=0; i<el.length; i++ ){
+          //debugger;
+          insert.call(this,el[i]);
+        }
+      }
+    });
+
+  }
+  if ( this._size/this._limit < this.lowerLimit ){
+    this._limit = this._limit / 2;
+
+    var insert = this.insert;
+    var oldStorage = this._storage;
+    this._storage = makeLimitedArray(this._limit);
+    oldStorage.each(function(el, key, list){
+      if( el !== undefined && el !== null){
+        console.log(list);
+        for( var i=0; i<el.length; i++ ){
+          //debugger;
+          this.insert(el[i]);
+        }
+      }
+    });
+
+  }
+
+
+
+
+}
 
 
 
