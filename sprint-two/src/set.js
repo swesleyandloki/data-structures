6e5f13341dae6a07162df8e49a059f08b500ee9d
swesleyandloki <swesleyandloki@gmail.com>
2014-10-30 20:59:50 -0700

    working set solution using array

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,23 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (this._storage === undefined){
+    this._storage = [];
+  }
+  this._storage.push(item);
+
 };
 
 setPrototype.contains = function(item){
+  return _.contains(this._storage,item);
 };
 
 setPrototype.remove = function(item){
+  var ind = _.indexOf(this._storage,item);
+  if( ind > -1 ){
+    this._storage[ind] = this._storage[this._storage.length-1];
+    this._storage.pop();
+  };
 };
 
 /*
